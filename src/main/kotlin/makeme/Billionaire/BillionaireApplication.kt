7f5d59e93930746d0bd7513f94a4986afcb141a3
hei5enbug <kbm0598@gmail.com>
2022-09-27 15:56:44 +0900

    Initial commit

---
+++ src/main/kotlin/makeme/Billionaire/BillionaireApplication.kt
@@ -0,0 +1,11 @@
+package makeme.Billionaire
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BillionaireApplication
+
+fun main(args: Array<String>) {
+	runApplication<BillionaireApplication>(*args)
+}
---
+++ src/test/kotlin/makeme/Billionaire/BillionaireApplicationTests.kt
@@ -0,0 +1,13 @@
+package makeme.Billionaire
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BillionaireApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
